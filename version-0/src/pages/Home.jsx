27a8cb2e5c074abb248103dd2cabd79abdfd90b1
catiecodes <catiehart#mac.com>
2025-06-11 10:21:32 -0700

    created and styled the home page to display the country cards

--- version-0/src/pages/Home.jsx
+++ version-0/src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import localData from "../../localData.js";
+
 function Home() {
+  console.log(localData[0], "localData");
   return (
-    <div>
-      <p>Home Page</p>
+    <div className="home-wrapper">
+      <div className="cards-wrapper">
+        {localData.map((item) => {
+          return (
+            <div className="country-card">
+              <img src={item.flags.png} />
+              <div className="card-text">
+                <h3>{item.name.common}</h3>
+                <p><span>Population:</span> {item.population}</p>
+                <p><span>Region:</span> {item.region}</p>
+                <p><span>Capital:</span> {item.capital}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
